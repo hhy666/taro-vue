44935f5ca282c93e2740d1ca6cfbf3ce46da8f69
hhy666 <h18835005270@163.com>
2020-07-21 14:42:51 +0800

    first commit

---
+++ src/pages/index/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <view class="index">
+    <text>{{ msg }}</text>
+  </view>
+</template>
+
+<script>
+import './index.styl'
+
+export default {
+  data () {
+    return {
+      msg: 'Hello world!'
+    }
+  }
+}
+</script>
